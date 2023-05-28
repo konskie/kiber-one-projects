9fdfc174942151be5241b5681c7ea24695f924a0
Nikolai <nikolaiko@mail.ru>
2023-05-28 15:59:36 +0600

    Final

--- ml-lit-app-evening/app/src/main/java/com/nikolai/mlkitcameraapp/helpers/ImageAnalizer.kt
+++ ml-lit-app-evening/app/src/main/java/com/nikolai/mlkitcameraapp/helpers/ImageAnalizer.kt
@@ -1,12 +1,36 @@
 package com.nikolai.mlkitcameraapp.helpers
 
+import android.annotation.SuppressLint
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.PoseDetectorOptionsBase
+import com.google.mlkit.vision.pose.PoseLandmark
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
 
 class ImageAnalizer: ImageAnalysis.Analyzer {
+    private val options = AccuratePoseDetectorOptions
+        .Builder()
+        .setDetectorMode(AccuratePoseDetectorOptions.STREAM_MODE)
+        .build()
+    private val detector = PoseDetection.getClient(options)
+    @SuppressLint("UnsafeOptInUsageError")
     override fun analyze(image: ImageProxy) {
-        println("салам")
+       val frameImage = image.image
+        if (frameImage!=null){
+            val imageForDetector = InputImage.fromMediaImage(frameImage, image.imageInfo.rotationDegrees)
+            val task = detector.process(imageForDetector)
+            task
+                .addOnSuccessListener {
+                    it.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
+                    image.close()
 
-        image.close()
+                }
+                .addOnFailureListener {
+                    image.close()
+                }
+        }
+
     }
 }
